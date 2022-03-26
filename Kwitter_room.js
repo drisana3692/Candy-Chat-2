198ff95fda4ec0b909a757097e77f01ea45b1844
Drisana <drisanabiswas369@gmail.com>
2022-03-26 16:08:20 -0400

    Add files via upload

---
+++ Kwitter_room.js
@@ -0,0 +1,22 @@
+var firebaseConfig = {
+
+    apiKey: "AIzaSyB_9kD6smjtlBPX4CyTTtHTIy4LrvBGIh0",
+  
+    authDomain: "my-awesome-project-d29ed.firebaseapp.com",
+  
+    databaseURL: "https://my-awesome-project-d29ed-default-rtdb.firebaseio.com",
+  
+    projectId: "my-awesome-project-d29ed",
+  
+    storageBucket: "my-awesome-project-d29ed.appspot.com",
+  
+    messagingSenderId: "638375718561",
+  
+    appId: "1:638375718561:web:7145d885cdd4977dee0ae0"
+  
+  };
+  
+  
+  // Initialize Firebase
+  
+  firebase.initializeApp(firebaseConfig);
